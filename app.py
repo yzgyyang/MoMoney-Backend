41530f3e71096e6acf482c33cd7880ffae301893
Guangyuan Yang <yzgyyang@outlook.com>
2019-09-14 20:25:21 -0400

    Add login functionality, rauth

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
 import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-import requests
-from flask import Flask, request
 import json
+import requests
+
+from os.path import join, dirname
+from flask import Flask, request, redirect
+from rauth import OAuth2Service
+
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 BASE_URL = "https://api.freshbooks.com"
 
+# Dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+
+# Helper: Compose URL with BASE_URL
+def url(s=""):
+    return BASE_URL + s
+
+
+# Freshbooks OAuth2
+freshbooks = OAuth2Service(
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    name="freshbooks",
+    authorize_url=url("/auth/oauth/authorize"),
+    access_token_url=url("/auth/oauth/token"),
+    base_url=url(),
+)
+
+redirect_uri = "https://momoney.ygy.io/test-auth"
 
 
 @app.route("/")
@@ -18,10 +40,15 @@ def index():
     return "Hello HTN '19!", 200
 
 
-@app.route("/login-succeeded")
-def login_succeeded():
-    token = request.form["code"]
-    return "Login succeeded! Your token is " + token
+@app.route("/login")
+def login():
+    params = {"scope": "identity",
+              "response_type": "code",
+              "redirect_uri": redirect_uri,
+              "duration": 'permanent'}
+
+    authorize_url = freshbooks.get_authorize_url(**params)
+    return redirect(authorize_url)
 
 
 @app.route("/test-auth")
@@ -34,9 +61,9 @@ def test_authentication():
         "client_id": os.getenv('CLIENT_ID'),
         "redirect_uri": os.getenv('REDIRECT_URL')
     }
-    auth_request = requests.post(BASE_URL + "/auth/oauth/token", json = auth_data)
+    auth_request = requests.post(BASE_URL + "/auth/oauth/token", json=auth_data)
     access_token_dict = json.loads(auth_request.text)
-    headers = {"Authorization":"Bearer " + access_token_dict["access_token"]}
+    headers = {"Authorization": "Bearer " + access_token_dict["access_token"]}
     identity_request = requests.get(BASE_URL + "/auth/api/v1/users/me", headers=headers)
     identity_dict = json.loads(identity_request.text)
     profile_dict = identity_dict["response"]["profile"]
@@ -45,4 +72,4 @@ def test_authentication():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', ssl_context='adhoc')
